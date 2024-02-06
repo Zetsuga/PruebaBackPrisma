022522f71ef9e4ea06166088ca7aa45aad91bd4f
JOSE ANTONIO ORS PLANAS <darkbear24@gmail.com>
2024-02-06 08:36:22 +0100

    Added new middleware to services

--- packages/services/controller/img.controller.ts
+++ packages/services/controller/img.controller.ts
@@ -16,23 +16,17 @@ cloudinary.v2.config({
   
 export const imgDownloader = async(request:UserRequest, reply:FastifyReply) => {
     try {
-        console.log('========>>>>>>>>>>>>> ',request['authUser'])
         const dataImg : any = await prisma.imgUser.findUnique({
             where:{
                 id_user:request['authUser']
             }
         })
         if(dataImg){ 
-            console.log('????????????????????????????????');
-            
             const result = cloudinary.v2.url(dataImg.id_img)
-
             reply.code(200).send(result)
         }
         reply.code(405).send('Cannot find a image')        
-    } catch (error: any) {
-        console.log(error);
-        
+    } catch (error: any) {        
         reply.code(200).send('Error on the server')
     }
 }
@@ -40,24 +34,30 @@ export const imgDownloader = async(request:UserRequest, reply:FastifyReply) => {
 
 export const imgUploader = async(request:UserRequest, reply:FastifyReply) => {
     try {
-        const fileData = await request.file();
-        const mimetype = fileData.mimetype
-        console.log('=====> ',mimetype)
-        const img = (await fileData.toBuffer()).toString('base64')
-        const data = `data:${mimetype};base64,${img}`;
-        const result = await cloudinary.v2.uploader.upload(data)
-        if(result){
-            console.log(request['authUser']);
-            console.log('-----> ',result.public_id)
-            const dataImg : any = await prisma.imgUser.create({
-                data:{
-                    id_user: request['authUser'],
-                    id_img : result.public_id
-                }
-            })
-            reply.code(200).send(result)
+
+        const dataUser = await prisma.imgUser.findUnique({
+            where:{
+                id_user:request['authUser']
+            }
+        })
+        if(!dataUser){
+            const fileData = await request.file();
+            const mimetype = fileData.mimetype
+            const img = (await fileData.toBuffer()).toString('base64')
+            const data = `data:${mimetype};base64,${img}`;
+            const result = await cloudinary.v2.uploader.upload(data)
+            if(result){
+                const dataImg : any = await prisma.imgUser.create({
+                    data:{
+                        id_user: request['authUser'],
+                        id_img : result.public_id
+                    }
+                })
+                reply.code(200).send('Image save')
+            }
+            reply.code(405).send('Not possible to save the image')
         }
-        reply.code(405).send('Not possible to save the image')
+        reply.code(405).send('U cant have more tahn one image in your profile')
     } catch (error: any) {        
         console.log(error)
         reply.code(200).send('Error on the server')
--- packages/services/src/routes/img.route.ts
+++ packages/services/src/routes/img.route.ts
@@ -1,6 +1,7 @@
 import { imgDownloader, imgUploader } from "../../controller/img.controller";
 import { checkTokenJWT, validateUser } from "../middleware/auth.middleware";
 import { FastifyInstance } from "fastify";
+import { checkLogo } from "../middleware/img.middleware";
 
 const imgRoute = async(fastify:FastifyInstance) => {
     fastify.decorateRequest('authUser','')
@@ -8,7 +9,6 @@ const imgRoute = async(fastify:FastifyInstance) => {
     fastify.route({
         method:'GET',
         url:'/',
-        //onResponse:[checkTokenJWT,validateUser],
         preHandler:[checkTokenJWT,validateUser],
         handler:imgDownloader
     })
@@ -16,7 +16,7 @@ const imgRoute = async(fastify:FastifyInstance) => {
     fastify.route({
         method:'POST',
         url:'/',
-        preHandler:[checkTokenJWT,validateUser],
+        preHandler:[checkTokenJWT,validateUser,checkLogo],
         handler:imgUploader
     })
 }
